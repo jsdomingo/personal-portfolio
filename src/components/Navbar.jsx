f0edfc5666b3885ccbba6abcb70b7cd2fe985e24
jsdomingo <53817072+jsdomingo@users.noreply.github.com>
2025-06-06 13:06:10 +1000

    refactor; hashrouter scroll to section for navlinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,23 @@ import styles from './Navbar.module.css';
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
+  const scrollToSection = (sectionId) => {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <div className={styles.navbarContainer}>
       <div className={styles.home}>
-        <Link to="/" className={styles.jesi}>Jesi</Link>
-        <Link to="/" className={styles.carey}>Carey</Link>
+        <Link to="/" onClick={() => scrollToSection('home')} className={styles.jesi}>Jesi</Link>
+        <Link to="/" onClick={() => scrollToSection('home')} className={styles.carey}>Carey</Link>
       </div>
       <div className={styles.navLinks}>
-        <Link to="/work" className={styles.work}>Work</Link>
-        <Link to="/info" className={styles.info}>Info</Link>
-        <Link to="/connect" className={styles.connect}>Connect</Link>
+        <Link to="/" onClick={() => scrollToSection('work')} className={styles.work}>Work</Link>
+        <Link to="/" onClick={() => scrollToSection('info')} className={styles.info}>Info</Link>
+        <Link to="/" onClick={() => scrollToSection('connect')} className={styles.connect}>Connect</Link>
       </div>
     </div>
   );
